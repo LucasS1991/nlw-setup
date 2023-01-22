8fcf33ec9622f2d569b728d61ac907070f1a4089
Lυcαร ємαиυєl <lucasjack02@gmail.com>
2023-01-22 15:16:54 -0300

    initial commit

---
+++ .vscode/assets/script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  /*const today = "25/01"*/
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso⚡")
+    return
+  }
+  alert("Adicionado com Sucesso👌")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits"))
+nlwSetup.setData(data)
+nlwSetup.load()
